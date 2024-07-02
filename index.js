65dc76ec742e6db361d60db5b2ed74c02a790abb
Shalini yadav <shaliniyadav0203@gmail.com>
2024-07-02 21:45:07 +0530

    Update index.js
    url

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.use(express.json());
 app.use(cors())
 
 const pool = new Pool({
-    user:process.env.USER_NAME,
-    host:process.env.HOST_NAME,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    dialect:process.env.DIALECT,
-    port:process.env.PORT_NUMBER
-
+    // user:process.env.USER_NAME,
+    // host:process.env.HOST_NAME,
+    // database:process.env.DB_NAME,
+    // password:process.env.DB_PASSWORD,
+    // dialect:process.env.DIALECT,
+    // port:process.env.PORT_NUMBER
+  connectionString: process.env.POSTGRES_URL,
 
 })
 
